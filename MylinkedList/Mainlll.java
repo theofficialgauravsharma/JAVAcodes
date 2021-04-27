68a8e1e5c9caffdc6c20c38a85a70f58d165f9c1
Gaurav Sharma <theofficialgauravsharma@gmail.com>
2021-04-27 13:24:21 +0530

    first commit

---
+++ MylinkedList/Mainlll.java
@@ -0,0 +1,15 @@
+package JAVApractice;
+
+public class Mainlll {
+
+	public static void main(String[] args) { 
+		
+		linkedList mll=new linkedList();
+		for(int i=0;i<10;i++) {
+			mll.add(i);
+		}
+		mll.print();
+
+	}
+
+}
